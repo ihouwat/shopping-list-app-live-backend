0c920d77ce3240fc7071a7fdec949c9ce9446106
ihouwat <houwat@gmail.com>
2020-05-16 16:08:05 -0400

    heroku

--- server.js
+++ server.js
@@ -15,10 +15,8 @@ const openmodal = require('./controllers/openModal');
 const db = knex({
   client: 'pg',
   connection: {
-    host : '127.0.0.1',
-    user : 'postgres',
-    password : 'Wisaal83',
-    database : 'quickshopper'
+    connectionString: process.env.DATABASE_URL,
+    ssl: true,
   }
 });
 
