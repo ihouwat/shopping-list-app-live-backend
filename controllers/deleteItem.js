2dda0fe12adc6e229f9e619f75648253b3649668
ihouwat <houwat@gmail.com>
2020-05-16 18:29:20 -0400

    db

--- controllers/deleteItem.js
+++ controllers/deleteItem.js
@@ -1,5 +1,4 @@
 const handleDeleteItem = (req, res, db) => {
-  console.log(req.body)
   const {name, id, note} = req.body.item
   const listName = req.body.listName.toLowerCase() // lowercase matches db table names
   // Identify the deleted item in the table and delete it
@@ -9,10 +8,11 @@ const handleDeleteItem = (req, res, db) => {
     return db.select().from(listName)
     .then(list => {
       // Send the table and table name to the front end
-      res.json({
-        listName: listName,
-        updatedList: list,
-      })
+      console.log(listName, list)
+      // res.json({
+      //   listName: listName,
+      //   updatedList: list,
+      // })
     })
   })
   .catch(err => res.status(400).json('could not delete item'))
