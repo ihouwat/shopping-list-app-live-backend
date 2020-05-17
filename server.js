6985bc8ccaac1e5e8a5a7587ce936a6ee0870104
ihouwat <houwat@gmail.com>
2020-05-17 14:15:56 -0400

    updated server and removed openModal controller

--- server.js
+++ server.js
@@ -10,7 +10,6 @@ const recoveritem = require('./controllers/recoverItem');
 const deleteallcompleted = require('./controllers/deleteAllCompleted');
 const recoverallcompleted = require('./controllers/recoverAllCompleted');
 const addnote = require('./controllers/addNote');
-const openmodal = require('./controllers/openModal');
 
 process.env.NODE_TLS_REJECT_UNAUTHORIZED = 0; 
 const db = knex({
@@ -25,7 +24,6 @@ const app = express(); // Start app
 app.use(cors()); // for CORS
 app.use(bodyParser.json()) // for parsing application/json
 app.use(bodyParser.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
-// app.get('/', (req, res) => {res.send('app is working')})
 
 app.get('/', (req, res) => {getitems.getItemsOnLoad(req, res, db)}); // Get lists
 app.post('/additem', (req, res) => {additem.handleAddItem(req, res, db)}) // Add item to grocery list
@@ -34,7 +32,6 @@ app.delete('/deleteitem', (req, res) => {deleteitem.handleDeleteItem(req, res ,d
 app.put('/recoveritem', (req, res) => {recoveritem.handleRecoverItem(req, res, db)}) // Recover item from completed list to grocery list
 app.delete('/deleteallcompleted', (req, res) => {deleteallcompleted.handleDeleteAllCompleted(req, res, db)}) // Delete all the completed items
 app.put('/recoverallcompleted', (req, res) => {recoverallcompleted.handleRecoverAllCompleted(req, res, db)}) // Recover all the completed items back to grocery list
-app.post('/openmodal', (req, res) => {openmodal.handleOpenModal(req, res, db)}) // Open modal and fetch item name note
 app.put('/addnote', (req, res) => {addnote.handleAddNote(req, res, db)}) // Add note to grocery item on modal close
 
 app.listen(process.env.PORT || 3000, () => {
