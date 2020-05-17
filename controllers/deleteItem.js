93a4466e2ae759899c93575c51e324e579089f72
ihouwat <houwat@gmail.com>
2020-05-16 22:56:27 -0400

    app changes

--- controllers/deleteItem.js
+++ controllers/deleteItem.js
@@ -2,19 +2,15 @@ const handleDeleteItem = (req, res, db) => {
   const {name, id, note} = req.body.item
   const listName = req.body.listName.toLowerCase() // lowercase matches db table names
   // Identify the deleted item in the table and delete it
-  db(listName).distinct('name')
-  .then (response => {
-    console.log(response)
-    db(listName).where('name', '=', name).del()
-    .then(response => {
-      // Get the table that was just updated
-      return db.select().from(listName)
-      .then(list => {
-        // Send the table and table name to the front end
-        res.json({
-          listName: listName,
-          updatedList: list,
-        })
+  db(listName).where('name', '=', name).del()
+  .then(response => {
+    // Get the table that was just updated
+    return db.select().from(listName)
+    .then(list => {
+      // Send the table and table name to the front end
+      res.json({
+        listName: listName,
+        updatedList: list,
       })
     })
   })
