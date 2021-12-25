13b88be3ecb319167a14ee9f98c16700808d81c6
Igor <houwat@gmail.com>
2021-12-25 09:48:08 -0500

    update readme

--- controllers/addItem.js
+++ controllers/addItem.js
@@ -2,7 +2,7 @@ const handleAddItem = (req, res, db) => {
   item = req.body
   number = Math.random().toString(36).substr(2, 9)// unique ID
   // Insert a new item in the database
-  db('items').insert([{name: item.name, id: number, note: ''} ])
+  db('items').insert([{name: item.name, id: number, note: '', count: 1} ])
   .then(function(result){
     // Get the items list from the database
     db.select().from('items')
