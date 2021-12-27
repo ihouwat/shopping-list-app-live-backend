f93b95dbb52e66200e77bb78d7620467dcc2ef13
Igor <houwat@gmail.com>
2021-12-26 19:36:40 -0500

    get count when recovering item

--- controllers/recoverItem.js
+++ controllers/recoverItem.js
@@ -1,8 +1,8 @@
 const handleRecoverItem = (req, res, db) => {
   db.transaction(trx => {
-    const {name, id, note} = req.body.item
+    const {name, id, note, count} = req.body.item
     // Readd item to grocery list and return id
-      return trx('items').insert([{name: name, id: id, note: note}]).returning('id')
+      return trx('items').insert([{name: name, id: id, note: note, count: count}]).returning('id')
       .then(id => {
         // Remove item from completed items list
         return trx('completeditems').where('id', '=', id[0]).del().returning('*')
