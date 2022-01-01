372c79215cae26926e0a07d848f03498dedf7528
Igor <houwat@gmail.com>
2021-12-31 20:04:24 -0500

    updateStoreCategories db update (not finished)

--- controllers/updateStoreCategories.js
+++ controllers/updateStoreCategories.js
@@ -1,12 +1,29 @@
 const handleUpdateStoreCategories = (req, res, db) => {
-  req.body.map(x => console.log(x));
-  // Get updated item from database
-  db.select().from('grocerystoremodel')
-  .then(groceryStoreModel => {
-    // Send updated item to the frontend
-    res.json ({ updatedStoreModel: groceryStoreModel })
-  })
-  .catch(err => res.status(400).json('could not update store categories'))
+
+  const updateFunction = (store, item) => {
+    return store[0]?.categories.map(category => {
+      if (category.category.includes(item.category)) {
+        let arr = category;
+        arr.items.push(item.itemName);
+        return category = arr;
+      } else return category;
+    })
+  }
+
+  try {
+    // Get updated item from database
+    req.body.forEach(item => {
+      db.select().from('grocerystoremodel').where('name', item.storeName)
+      .then(res => updateFunction(res, item))
+      .then(push => db.select().from('grocerystoremodel').where('name', item.storeName).update('categories', JSON.stringify(push)).returning('*'))
+      .then(()=>  db.select().from('grocerystoremodel').orderBy('id', 'asc'))
+      .then(groceryStoreModel => res.json ({ updatedStoreModel: groceryStoreModel })
+      )
+    })
+  }
+  catch {
+    res.status(400).json('could not update store categories')
+  }
 }
 
 module.exports = {
