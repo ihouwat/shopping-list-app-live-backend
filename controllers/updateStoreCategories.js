e5fc0077f5ff15287b03d2cdd9c43bc81181532e
Igor <houwat@gmail.com>
2022-01-01 16:10:17 -0500

    finish updateStoreCategories.js

--- controllers/updateStoreCategories.js
+++ controllers/updateStoreCategories.js
@@ -1,29 +1,50 @@
 const handleUpdateStoreCategories = (req, res, db) => {
+  // const [item, stores] = req.body;
+  const item = req.body.itemName;
+  const stores = req.body.stores;
 
-  const updateFunction = (store, item) => {
-    return store[0]?.categories.map(category => {
-      if (category.category.includes(item.category)) {
-        let arr = category;
-        arr.items.push(item.itemName);
-        return category = arr;
-      } else return category;
+  // Returns array of item name, store name, and updated categories for that store's model
+  const updateModel = (model, storesObj) => {
+    let tripleArray = []
+    storesObj.map(store => model.map(storeModel => {
+      if(store.storeName === storeModel.name) {
+        let categoryToUpdate = storeModel.categories.find(cat => cat.category === store.category);
+        if(!categoryToUpdate.items.find(it => it.toLowerCase() === item.toLowerCase())) {
+          updatedItems = {...categoryToUpdate, ...categoryToUpdate.items.push(item)};
+          tripleArray.push([item, store.storeName, storeModel.categories.map(cat => cat === categoryToUpdate ? cat = updatedItems : cat)])
+        }
+      }
+    }))
+    return tripleArray;
+  }
+
+  // Updates all affected store categories and adds the item name to the groceries template table for autocomplete purposes
+  const batchUpdate = (table, collection) => {
+    return db.transaction(trx => {
+      const queries = collection.map(tuple => {
+        [itemName, storeName, updatedCategories] = tuple;
+        return db(table)
+          .where('name', storeName)
+          .update('categories', JSON.stringify(updatedCategories))
+          .transacting(trx)
+      });
+      return Promise.all(queries)
+        .then(() => {
+          return db('groceriestemplate').where('name', item)
+            .then((rows) => {
+              if(rows.length === 0) return db('groceriestemplate').insert({name: item, count: 0});
+            })
+        })
+        .then(trx.commit)
+        .catch(trx.rollback)
     })
   }
 
-  try {
-    // Get updated item from database
-    req.body.forEach(item => {
-      db.select().from('grocerystoremodel').where('name', item.storeName)
-      .then(res => updateFunction(res, item))
-      .then(push => db.select().from('grocerystoremodel').where('name', item.storeName).update('categories', JSON.stringify(push)).returning('*'))
-      .then(()=>  db.select().from('grocerystoremodel').orderBy('id', 'asc'))
-      .then(groceryStoreModel => res.json ({ updatedStoreModel: groceryStoreModel })
-      )
-    })
-  }
-  catch {
-    res.status(400).json('could not update store categories')
-  }
+  db.select().from('grocerystoremodel')
+  .then(res => updateModel(res, stores))
+  .then(collection => batchUpdate('grocerystoremodel', collection))
+  .then(() => db.select().from('grocerystoremodel').orderBy('id', 'asc'))
+  .then(updatedModel => res.send({updatedModel  : updatedModel}))
 }
 
 module.exports = {
