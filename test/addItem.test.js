35e9c22dbdb19d78eb390d365cdda1d972fef0be
Igor <houwat@gmail.com>
2023-03-12 14:20:21 -0400

    add mortest for handleAddItem caught error

--- test/addItem.test.js
+++ test/addItem.test.js
@@ -1,15 +1,6 @@
 const addItem = require('../controllers/addItem');
 const db = require('../server');
 
-const expected = [
-  {
-    "name": "Pasta",
-    "id": "50jzy696i",
-    "note": "",
-    "count": 1
-  }
-];
-
 jest.mock('../server', () => {
   const mKnex = { 
     count: jest.fn(),
@@ -28,13 +19,25 @@ jest.mock('../server', () => {
 });
 
 const req = { body: { name: 'item' }};
-const res = {};
+const res = { status: (status) => ({statusCode: status, json: (data) => data}), statusCode: 200};
 
 describe('add item', () => {
-  
-  test('', async () => {
-    db().then.mockResolvedValueOnce(expected)
-    const actual = await addItem.handleAddItem(req, res, db())
+  test('should return item after successful insert', async () => {
+    const expected = [{
+      "name": "Pasta",
+      "id": "50jzy696i",
+      "note": "",
+      "count": 1
+    }];
+    db().then.mockResolvedValueOnce(expected);
+    const actual = await addItem.handleAddItem(req, res, db());
+    expect(actual).toEqual(expected);
+  });
+
+  test('should return an error message when the database fails to insert the item', async () => {
+    db().then.mockRejectedValueOnce('error');
+    const actual = await addItem.handleAddItem(req, res, db());
+    const expected = {errorMessage: 'Could not add item to list.', statusCode: 400};
     expect(actual).toEqual(expected);
   });
 });
