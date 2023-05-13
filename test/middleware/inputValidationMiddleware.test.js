bd9508a24b13c7ace6f08e9524a083568291ac43
Igor <houwat@gmail.com>
2023-05-13 13:53:21 -0400

    add unit test for inputValidationMiddleware

---
+++ test/middleware/inputValidationMiddleware.test.js
@@ -0,0 +1,28 @@
+const validateSchema = require('../../middleware/inputValidationMiddleware');
+
+describe('inputValidationMiddleware', () => {
+	const schema = {
+		validate: jest.fn()
+	};
+	const req = {
+		body: {}
+	};
+	const res = {
+		status: jest.fn().mockReturnThis(),
+		json: jest.fn()
+	};
+	const next = jest.fn();
+
+	it('should call next if no error', () => {
+		schema.validate.mockReturnValue({});
+		validateSchema(schema)(req, res, next);
+		expect(next).toHaveBeenCalled();
+	});
+
+	it('should return 400 if error', () => {
+		schema.validate.mockReturnValueOnce({ error: { details: [{ message: 'error' }]}});
+		validateSchema(schema)(req, res, next);
+		expect(res.status).toHaveBeenCalledWith(400);
+		expect(res.json).toHaveBeenCalledWith({ error: 'error' });
+	});
+});
