fe13082d94fb329fd11e55bc4d883841071891aa
Igor <houwat@gmail.com>
2023-06-18 14:18:52 -0400

    add rate limiting

--- server.js
+++ server.js
@@ -8,16 +8,27 @@ const swaggerUi = require('swagger-ui-express');
 const swaggerSpec = require('./config/swaggerFile');
 const validateSchema = require('./src/middleware/inputValidationMiddleware');
 const schemas = require('./src/schemas/validationSchemas');
+const rateLimit = require('express-rate-limit');
 
 process.env.NODE_TLS_REJECT_UNAUTHORIZED = 0; 
 
 const app = express(); // Start app
+app.set('trust proxy', 1); // Trust first proxy	for rate limiter
 const PORT = process.env.PORT || 3000;
+const limiter = rateLimit({
+	windowMs: 1 * 60 * 1000, // 1 minutes
+	max: 1, // Limit each IP to 120 requests per `window`
+	message: 'Too many requests from this IP',
+	standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+	legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+});
+
 app.use(morgan('combined', { stream: winston.stream })); // for logging
 app.use(cors()); // for CORS
 app.use(bodyParser.json()); // for parsing application/json
 app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec)); // Swagger API documentation
+app.use(limiter); // Rate limiter
 
 // Routes
 app.use('/', routes.getItems); // Get lists
